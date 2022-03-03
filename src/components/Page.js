1311ce642b2ed5317d806ae5fe4a1d6d02395c3a
Kofeinich <59574446+Kofeinich@users.noreply.github.com>
2022-03-03 06:53:50 +0300

    feat: svg background added

--- src/components/Page.js
+++ src/components/Page.js
@@ -5,26 +5,13 @@ import InstagramIcon from "./source/InstagramIcon";
 import GitHubIcon from "./source/GitHubIcon";
 import styled from "styled-components";
 import CustomImage from "./source/Image";
+import SVGForME from "./source/SVGForME";
 
 
 const StyledSection = styled.section`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
   height: 100vh;
   ::before {
-    background: repeating-linear-gradient(210deg, rgb(10, 134, 89) 40px,
-    rgba(106, 243, 195, 0.53) 80px, rgb(115, 113, 113) 120px,
-    rgba(238, 80, 10, 0.7) 160px, rgb(13, 169, 110) 200px,
-    rgb(13, 169, 110) 240px, rgba(106, 243, 195, 0.53) 280px,
-    rgb(8, 117, 80) 300px),
-    repeating-linear-gradient(-210deg, rgb(8, 117, 80) 30px,
-    rgba(238, 80, 10, 0.8) 60px, rgb(115, 113, 113) 90px,
-    rgba(238, 80, 10, 0.7) 120px, rgba(238, 80, 10, 0.8) 150px,
-    rgb(13, 169, 110) 180px, rgba(238, 80, 10, 0.8) 210px,
-    rgb(8, 117, 80) 230px),
-    repeating-linear-gradient(23deg, rgba(25, 77, 59, 0.75) 50px, rgba(238, 80, 10, 0.8) 100px,
-    #9b9a9a 150px, rgba(238, 80, 10, 0.7) 200px, #0da96e 250px,
-    #0da96e 300px, rgba(238, 80, 10, 0.8) 350px, rgba(50, 134, 103, 0.75) 370px) repeat;
+    background: peachpuff; 
     content: "";
     top: 0;
     left: 0;
@@ -50,7 +37,7 @@ const StyledSection = styled.section`
       text-align: center;
       width: 100%;
       height: 100vh;
-      min-height: 620px;
+      min-height: 700px;
       min-width: 300px;
     }
   }
@@ -146,6 +133,7 @@ if (window.innerWidth > 520){
 function Page(){
     return <StyledSection>
         <StyledSectionInside>
+            <SVGForME/>
             <StyledBox1>
             </StyledBox1>
             <StyledBox2>
